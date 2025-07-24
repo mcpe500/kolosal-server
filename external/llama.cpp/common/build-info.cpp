b6c3a18e9e38a718d71f4d1ccf0ce447d9be3c4d
Rifky Bujana Bisri <rifkybujanabisri@gmail.com>
2025-07-24 16:58:55 +0700

    Update build configuration for MuPDF and update llama build info

--- external/llama.cpp/common/build-info.cpp
+++ external/llama.cpp/common/build-info.cpp
@@ -1,4 +1,4 @@
-int LLAMA_BUILD_NUMBER = 124;
-char const *LLAMA_COMMIT = "16dbfa6b";
-char const *LLAMA_COMPILER = "cc (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0";
-char const *LLAMA_BUILD_TARGET = "x86_64-linux-gnu";
+int LLAMA_BUILD_NUMBER = 202;
+char const *LLAMA_COMMIT = "43928c07";
+char const *LLAMA_COMPILER = "MSVC 19.43.34810.0";
+char const *LLAMA_BUILD_TARGET = "x64";
